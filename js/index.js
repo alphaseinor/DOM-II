827bac5b64f256c9bad7d8dc94a2b7f5f3efe2a8
Brian Hague <alphaseinor@gmail.com>
2019-09-10 20:33:54 -0500

    added wheel event and drag event

--- js/index.js
+++ js/index.js
@@ -12,16 +12,45 @@ allMenuItems.forEach((a) => {
 })
 
 // * [ ] `keydown`
+const bodyKeyDown = document.querySelector('body')
+const funBusLogo = document.querySelector('.logo-heading')
 
+bodyKeyDown.addEventListener("keydown", (event) => {
+  if(event.keyCode === 70){
+    funBusLogo.style.fontWeight = "bold"
+    console.log("You just put the F in Fun Bus!")
+  }
+})
 
 
 // * [ ] `wheel`
+const funBusImg = document.querySelector('.intro img')
+const header = document.querySelector('.main-navigation')
 
+header.style.zIndex = "1000"
+
+funBusImg.addEventListener("wheel", z => {
+  console.log("scroll", z.deltaY)
+  if(z.deltaY > 0){
+    //scroll up
+    funBusImg.style.transform = "scale(.5)"
+  }else{
+    //scroll down
+    funBusImg.style.transform = "scale(1)"
+  }
+})
 
 
 // * [ ] `drag / drop`
 
+const images = document.querySelectorAll("img")
 
+images.forEach((x) => {
+  x.addEventListener("drag", (event) => {
+    event.preventDefault()
+    console.log("denied")
+  })
+})
 
 // * [ ] `load`
 
