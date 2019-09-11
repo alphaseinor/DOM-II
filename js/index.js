193c5ce2bc4c5ab9a3160fe73b2f6d2976dd8d9b
Brian Hague <alphaseinor@gmail.com>
2019-09-10 19:22:46 -0500

    added bold style to nav-link class

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+// * [ ] `mouseover`
+const allMenuItems = document.querySelectorAll('.nav-link')
+
+allMenuItems.forEach((a) => {
+  a.addEventListener("mouseenter", (event) => {
+    console.log("mouseenter")
+    a.style.fontWeight = "bold"
+  })
+})
+
+// * [ ] `keydown`
+
+
+
+// * [ ] `wheel`
+
+
+
+// * [ ] `drag / drop`
+
+
+
+// * [ ] `load`
+
+
+
+// * [ ] `focus`
+
+
+
+// * [ ] `resize`
+
+
+
+// * [ ] `scroll`
+
+
+
+// * [ ] `select`
+
+
+
+// * [ ] `dblclick`
